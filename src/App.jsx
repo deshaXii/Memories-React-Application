32a42e8924e44844dab078248e5e497f30538157
mustafa fathi <mustafa.fathi7@gmail.com>
2021-06-28 03:26:57 +0200

    create app style with matiral style and style our form and posts and post components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import memories from "./images/memories.png";
+
+import useStyles from "./styles";
+
 const App = () => {
+  const classes = useStyles()
   return (
-    <div className="App">
-      <span>App</span>
-    </div>
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography  className={classes.heading} align="center" variant="h2">
+          Memories
+        </Typography>
+        <img src={memories}  className={classes.image} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
